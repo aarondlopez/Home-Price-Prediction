5c1b428154e10d5e4ac511193c7329894dd99971
aarondlopez <aarondlopez@gmail.com>
2017-06-08 16:02:27 -0700

    random forest

--- Ch. 7 Trees and Text/StevensTree.R
+++ Ch. 7 Trees and Text/StevensTree.R
@@ -12,7 +12,8 @@ library(rpart)
 library(rpart.plot)
 
 # create CART model
-StevensTree = rpart(Reverse~Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = Train, method="class", control=rpart.control(minbucket=25))
+StevensTree = rpart(Reverse~Circuit + Issue + Petitioner + Respondent + LowerCourt + 
+                      Unconst, data = Train, method="class", control=rpart.control(minbucket=25))
 prp(StevensTree)
 PredictCART = predict(StevensTree, newdata=Test, type="class")
 table(Test$Reverse, PredictCART)
@@ -30,3 +31,32 @@ PredictROC
 pred = prediction(PredictROC[,2], Test$Reverse)
 perf = performance(pred, "tpr", "fpr")
 plot(perf)
+
+# Create a random forest model
+library(randomForest)
+StevensForest = randomForest(Reverse~Circuit + Issue + Petitioner + Respondent + 
+                               LowerCourt + Unconst, data=Train, nodesize=25, ntree=200)
+#Warning message:
+#In randomForest.default(m, y, ...) :
+  #The response has five or fewer unique values.  Are you sure you want to do regression?
+
+# Change our y variable to a factor
+Train$Reverse = as.factor(Train$Reverse)
+Test$Reverse = as.factor(Test$Reverse)
+StevensForest = randomForest(Reverse~Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data=Train, nodesize=25, ntree=200)
+
+# Use the predict function on our model using the test data set to determine the accuracy of our model
+PredictForest = predict(StevensForest, newdata = Test)
+table(Test$Reverse, PredictForest)
+#PredictForest
+#   0  1
+#0 40 37
+#1 18 75
+(40+74)/(40+37+18+75)
+#[1] 0.6705882
+# The model accurately predicted 67% of the actual outcomes, better than the CART Model at 65%
+
+
+
+
+
